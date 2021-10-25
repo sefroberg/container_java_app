6e20d2e64707e2eeb8d9c32d93b39e9e202b3c24
Scott Froberg <sfroberg@redhat.com>
2021-10-25 16:27:03 -0400

    Simplify code usage

--- SimpleApp/SimpleApp.java
+++ SimpleApp/SimpleApp.java
@@ -1,11 +1,13 @@
-import java.lang.Runtime.Version;
+import java.util.Properties;
+
 
 public class SimpleApp
 {
     public static void main(String[] args)
     {
         System.out.println("This is a simple java app.");
-        Version version = java.lang.Runtime.version();
+        String version = System.getProperty("java.version");
         System.out.println("Java Version = " + version);
+
     }
 }
